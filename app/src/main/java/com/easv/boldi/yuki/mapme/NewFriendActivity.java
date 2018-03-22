a662f810a71c668c1a60c2d7a17882d099ea1054
Carlos <carl7755@easv365.dk>
2018-03-22 10:58:29 +0100

    changed some colors in add friend activity

--- app/src/main/java/com/easv/boldi/yuki/mapme/NewFriendActivity.java
+++ app/src/main/java/com/easv/boldi/yuki/mapme/NewFriendActivity.java
@@ -3,7 +3,6 @@ package com.easv.boldi.yuki.mapme;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +17,6 @@ public class NewFriendActivity extends AppCompatActivity {
     private EditText mWebsiteTxt;
     private EditText mBirthdayTxt;
     private EditText mPhoneText;
-    private Toolbar toolbar;
 
 
     @Override
@@ -53,33 +51,11 @@ public class NewFriendActivity extends AppCompatActivity {
             }
         });
         this.setTitle(null);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
 
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 
 }
