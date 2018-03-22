618d7be914b541c96118aed5fdd42ddf4efba526
Carlos <carl7755@easv365.dk>
2018-03-22 10:43:59 +0100

    refactored add friend view

--- app/src/main/java/com/easv/boldi/yuki/mapme/NewFriendActivity.java
+++ app/src/main/java/com/easv/boldi/yuki/mapme/NewFriendActivity.java
@@ -11,12 +11,14 @@ import android.widget.EditText;
 public class NewFriendActivity extends AppCompatActivity {
 
     private Button mSaveButton;
+    private Button mCancelButton;
     private EditText mNameTxt;
     private EditText mAddressTxt;
     private EditText mEmailText;
     private EditText mWebsiteTxt;
     private EditText mBirthdayTxt;
     private EditText mPhoneText;
+    private Toolbar toolbar;
 
 
     @Override
@@ -33,6 +35,14 @@ public class NewFriendActivity extends AppCompatActivity {
         mPhoneText = findViewById(R.id.phone_Info);
 
         mSaveButton = findViewById(R.id.saveBtn);
+        mCancelButton = findViewById(R.id.cancelBtn);
+
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         mSaveButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -42,18 +52,34 @@ public class NewFriendActivity extends AppCompatActivity {
 
             }
         });
+        this.setTitle(null);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
 
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
