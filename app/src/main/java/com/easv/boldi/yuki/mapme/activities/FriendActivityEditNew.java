7e3f769f825200a37bffa2837b44b42806047730
kopicon <bold0038@easv365.dk>
2018-04-09 03:30:46 +0200

    Map show marker fix

--- app/src/main/java/com/easv/boldi/yuki/mapme/activities/FriendActivityEditNew.java
+++ app/src/main/java/com/easv/boldi/yuki/mapme/activities/FriendActivityEditNew.java
@@ -1,9 +1,14 @@
 package com.easv.boldi.yuki.mapme.activities;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -12,12 +17,22 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.content.ContentValues.TAG;
+import static com.easv.boldi.yuki.mapme.MainActivity.mLocationPermissionGranted;
 
 public class FriendActivityEditNew extends AppCompatActivity {
 
@@ -29,6 +44,10 @@ public class FriendActivityEditNew extends AppCompatActivity {
     private static final int REQUEST_CODE = 8;
     public String lat, lng;
     public EditText mAddressTxt;
+    private LocationManager mLocation;
+    private double latc;
+    private double lngc;
+
 
     public void initOnTextChangedListener() {
 
@@ -39,6 +58,7 @@ public class FriendActivityEditNew extends AppCompatActivity {
                 return false;
             }
         });
+
         mBirthdayTxt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -56,7 +76,7 @@ public class FriendActivityEditNew extends AppCompatActivity {
                 Log.d(TAG, "afterTextChanged: " + number);
                 if (number.length() == 4 && !number.contains("/")) {
 
-                    number = String.format("%s/%s/", s.toString().substring(0, 2),
+                    number = String.format("%s/%s", s.toString().substring(0, 2),
                             s.toString().substring(2, 4));
                     mBirthdayTxt.setText(number);
                     mBirthdayTxt.setSelection(number.length());
@@ -72,7 +92,19 @@ public class FriendActivityEditNew extends AppCompatActivity {
 
         });
     }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLocation = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            Location curentLocation = mLocation.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            latc = curentLocation.getLatitude();
+            lngc = curentLocation.getLongitude();
 
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: Security Exeption" + e.getMessage());
+        }
+    }
 
     /**
      * Generalized method for asking permission. Can pass any array of permissions
@@ -127,6 +159,8 @@ public class FriendActivityEditNew extends AppCompatActivity {
         }
     }
 
+
+
     public boolean checkStringIfNull(String string) {
         return !string.equals("");
     }
@@ -137,11 +171,11 @@ public class FriendActivityEditNew extends AppCompatActivity {
         Geocoder geocoder = new Geocoder(this);
         List<Address> list = new ArrayList<>();
         try {
-            list = geocoder.getFromLocationName(searchString, 1);
+            list = geocoder.getFromLocationName(searchString, 2);
         } catch (IOException e) {
             Log.e(TAG, "geoLocate: IOException" + e.getMessage());
         }
-        if (list.size() > 0) {
+        if (list.size() == 1) {
             Address address = list.get(0);
             Log.d(TAG, "geoLocate: Found a location " + address.toString());
 
@@ -149,7 +183,31 @@ public class FriendActivityEditNew extends AppCompatActivity {
             Double longitude = address.getLongitude();
             lat = Double.toString(latitude);
             lng = Double.toString(longitude);
+        }else if (list.size() == 2){
+            float[] results = new float[list.size()];
+            Address address_1 = list.get(0);
+            Address address_2 = list.get(1);
+
+
+
+            Double friendLat1 = address_1.getLatitude();
+            Double friendLng1 = address_1.getLongitude();
+            Double friendLat2 = address_2.getLatitude();
+            Double friendLng2 = address_2.getLongitude();
+
+
+            Location.distanceBetween(friendLat1,friendLng1,latc,lngc,results);
+            Location.distanceBetween(friendLat2,friendLng2,latc,lngc,results);
+
+            if(results[0]< results[1]){
+                lat = Double.toString(friendLat1);
+                lng = Double.toString(friendLng1);
+            }else if(results[1]<results[0]){
+                lat = Double.toString(friendLat2);
+                lng = Double.toString(friendLng2);
+            }
         }
     }
+    }
 
-}
+
